4f14c693c6e97dec604d81bfbcd764e1f1f9aec2
Gabriel Hernandes <ghh.hernandes@gmail.com>
2022-09-03 08:59:17 -0300

    added new files

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,42 @@
+vim.g.ghhernandes_colorscheme = "gruvbox"
+--vim.g.ghhernandes_colorscheme = "tokyonight"
+
+function MyColors()
+    vim.g.gruvbox_contract_dark = 'hard'
+    vim.g.tokyonight_transparent_sidebar = true
+    vim.g.gruvbox_invert_selection = '0'
+    vim.opt.background = "dark"
+
+    vim.cmd("colorscheme " .. vim.g.ghhernandes_colorscheme)
+
+    local hl = function(thing, opts)
+        vim.api.nvim_set_hl(0, thing, opts)
+    end
+
+    hl("SignColumn", {
+        bg = "none",
+    })
+
+    hl("ColorColumn", {
+        ctermbg = 0,
+        bg = "#555555",
+    })
+
+    hl("CursorLineNR", {
+        bg = "None"
+    })
+
+    hl("Normal", {
+        bg = "none"
+    })
+
+    hl("LineNr", {
+        fg = "#5eacd3"
+    })
+
+    hl("netrwDir", {
+        fg = "#5eacd3"
+    })
+end
+
+MyColors()

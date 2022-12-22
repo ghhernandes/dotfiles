f80ab60ec2e01a821b02a9096cafce4cea566bd3
Gabriel Hernandes <ghh.hernandes@gmail.com>
2022-12-21 23:13:15 -0300

    remap ESC to C-c

--- nvim/lua/ghhernandes/remap.lua
+++ nvim/lua/ghhernandes/remap.lua
@@ -3,6 +3,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")

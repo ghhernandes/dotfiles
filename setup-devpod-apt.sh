338032328542df4d6d9a96763789885e7a2d578e
Gabriel Hernandes <ghh.hernandes@gmail.com>
2025-08-03 13:04:02 +0000

    devpod: neovim AppImage

--- setup-devpod-apt.sh
+++ setup-devpod-apt.sh
@@ -6,11 +6,22 @@ mkdir -p "$XDG_CONFIG_HOME"
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME/nvim"
 ln -sf "$PWD/tmux" "$XDG_CONFIG_HOME/tmux"
 
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH="x86_64"
+elif [ "$ARCH" = "aarch64" ]; then
+    ARCH="arm64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+
+fi
+
 packages=(
+    curl
     ripgrep
     fzf
     tmux
-    neovim
 )
 
 for package in "${packages[@]}"; do
@@ -18,6 +29,18 @@ for package in "${packages[@]}"; do
         apt-get install -y "$package"
 done
 
-# Neovim Package manager
+# Neovim AppImage (no FUSE required)
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-$ARCH.appimage
+chmod u+x nvim-linux-$ARCH.appimage
+
+./nvim-linux-$ARCH.appimage --appimage-extract
+
+mkdir -p /opt/nvim
+mv squashfs-root /opt/nvim/nvim
+
+ln -s /opt/nvim/nvim/AppRun /usr/local/bin/nvim
+
+# Neovim Package Manager
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim

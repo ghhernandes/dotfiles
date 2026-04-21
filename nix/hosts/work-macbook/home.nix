787bdb59d26d5995fb052e30c20551cd39783d30
Gabriel Hernandes <gabriel.hernandes@nubank.com.br>
2026-04-21 19:35:34 -0300

    nix: disable git management on work-macbook

--- nix/hosts/work-macbook/home.nix
+++ nix/hosts/work-macbook/home.nix
@@ -1,8 +1,11 @@
-{ self, ... }:
+{ self, lib, ... }:
 
 {
   imports = with self.homeModules; [
     common
     cli
   ];
+
+  # Use existing ~/.gitconfig instead of home-manager managed git
+  programs.git.enable = lib.mkForce false;
 }

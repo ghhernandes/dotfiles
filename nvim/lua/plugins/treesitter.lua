3ce861ec9ff20db60124b4032860ef917172ff6b
Gabriel Hernandes <ghh.hernandes@gmail.com>
2026-04-21 21:37:08 -0300

    nvim: simplify treesitter config, use default highlight/indent

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,57 +1,13 @@
-require 'nvim-treesitter.configs'.setup {
-    -- Parsers are managed by Nix (nvim-treesitter.withAllGrammars)
-    -- Do NOT let TreeSitter try to install parsers (Nix store is read-only)
-    ensure_installed = {},
-    sync_install = false,
-    auto_install = false,
+-- Parsers are managed by Nix (nvim-treesitter.withAllGrammars)
+-- Treesitter highlight/indent are enabled automatically by the plugin
 
-    highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
-
-    indent = { enabled = true },
-
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-        },
-    },
-
-    refactor = {
-        smart_rename = {
-            enable = true,
-            keymaps = {
-                smart_rename = "<leader>grr",
-            },
-        },
-
-        highlight_definitions = {
-            enable = true,
-            clear_on_cursor_move = true,
-        },
-
-        navigation = {
-            enable = true,
-            -- Assign keymaps to false to disable them, e.g. `goto_definition = false`.
-            keymaps = {
-                goto_definition = "gd",
-                list_definitions = "gD",
-                list_definitions_toc = "gO",
-                goto_next_usage = "<a-*>",
-                goto_previous_usage = "<a-#>",
-            },
-        },
-
-    }
-}
+-- Incremental selection
+vim.keymap.set('n', '<C-space>', function()
+    require('nvim-treesitter.incremental_selection').init_selection()
+end)
+vim.keymap.set('v', '<C-space>', function()
+    require('nvim-treesitter.incremental_selection').node_incremental()
+end)
+vim.keymap.set('v', '<bs>', function()
+    require('nvim-treesitter.incremental_selection').node_decremental()
+end)

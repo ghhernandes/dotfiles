7f66ec32991ce595b67752cbd45a785b1f88e314
Gabriel Hernandes <ghh.hernandes@gmail.com>
2026-06-13 09:36:38 -0300

    remote-access: remove sunshine streaming service
    Drop the sunshine service and its firewall port openings; keep
    tailscale and the firewall enabled.

--- nix/system/remote-access.nix
+++ nix/system/remote-access.nix
@@ -3,30 +3,5 @@ _:
 {
   services.tailscale.enable = true;
 
-  services.sunshine = {
-    enable = true;
-    autoStart = true;
-    capSysAdmin = true;
-    openFirewall = true;
-  };
-
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [
-      47984
-      47989
-      47990
-      48010
-    ];
-    allowedUDPPortRanges = [
-      {
-        from = 47998;
-        to = 48000;
-      }
-      {
-        from = 8000;
-        to = 8010;
-      }
-    ];
-  };
+  networking.firewall.enable = true;
 }

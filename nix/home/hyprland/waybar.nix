e30bac501a5fba45b5080bc3dcd76d63b2c343f7
Gabriel Hernandes <ghh.hernandes@gmail.com>
2026-08-11 07:57:41 -0300

    hyprland: refresh waybar layout and dunst styling

--- nix/home/hyprland/waybar.nix
+++ nix/home/hyprland/waybar.nix
@@ -7,27 +7,52 @@ _:
       mainBar = {
         layer = "top";
         position = "top";
-        height = 30;
+        height = 26;
+        spacing = 0;
+        reload_style_on_change = true;
+
         modules-left = [
           "hyprland/workspaces"
           "hyprland/window"
         ];
         modules-center = [
-          "custom/gh-pomodoro"
           "clock"
         ];
         modules-right = [
-          "pulseaudio"
-          "network"
+          "custom/focus"
+          "group/tray-expander"
           "bluetooth"
+          "network"
+          "pulseaudio"
           "cpu"
           "memory"
           "battery"
         ];
 
         "hyprland/workspaces" = {
-          format = "{id}";
           on-click = "activate";
+          format = "{icon}";
+          format-icons = {
+            default = "";
+            active = "󱓻";
+            "1" = "1";
+            "2" = "2";
+            "3" = "3";
+            "4" = "4";
+            "5" = "5";
+            "6" = "6";
+            "7" = "7";
+            "8" = "8";
+            "9" = "9";
+            "10" = "0";
+          };
+          persistent-workspaces = {
+            "1" = [ ];
+            "2" = [ ];
+            "3" = [ ];
+            "4" = [ ];
+            "5" = [ ];
+          };
         };
 
         "hyprland/window" = {
@@ -35,84 +60,150 @@ _:
         };
 
         clock = {
-          format = "{:%b %d %I:%M %p}";
+          format = "{:L%a %H:%M}";
+          format-alt = "{:L%d %b W%V %Y}";
+          tooltip = false;
         };
 
         cpu = {
-          format = "󰻠 {usage}%";
-        };
-
-        temperature = {
-          format = "󰔏 {temperatureC}°C";
-          critical-threshold = 80;
-          format-critical = "󰸁 {temperatureC}°C";
+          interval = 5;
+          format = "󰻠";
+          tooltip-format = "CPU {usage}%";
+          on-click = "kitty --class btop-float btop";
         };
 
         memory = {
-          format = "󰍛 {}%";
+          interval = 5;
+          format = "󰍛";
+          tooltip-format = "RAM {percentage}% ({used:0.1f}G / {total:0.1f}G)";
+          on-click = "kitty --class btop-float btop";
         };
 
         battery = {
-          format = "{icon} {capacity}%";
-          format-charging = "󰂄 {capacity}%";
-          format-plugged = "󰚥 {capacity}%";
-          format-icons = [
-            "󰂎"
-            "󰁺"
-            "󰁻"
-            "󰁼"
-            "󰁽"
-            "󰁾"
-            "󰁿"
-            "󰂀"
-            "󰂁"
-            "󰂂"
-            "󰁹"
-          ];
+          format = "{capacity}% {icon}";
+          format-charging = "{capacity}% {icon}";
+          format-full = "{capacity}% 󰂅";
+          format-icons = {
+            charging = [
+              "󰢜"
+              "󰂆"
+              "󰂇"
+              "󰂈"
+              "󰢝"
+              "󰂉"
+              "󰢞"
+              "󰂊"
+              "󰂋"
+              "󰂅"
+            ];
+            default = [
+              "󰁺"
+              "󰁻"
+              "󰁼"
+              "󰁽"
+              "󰁾"
+              "󰁿"
+              "󰂀"
+              "󰂁"
+              "󰂂"
+              "󰁹"
+            ];
+          };
+          tooltip-format-discharging = "{power:>1.0f}W↓ {capacity}%";
+          tooltip-format-charging = "{power:>1.0f}W↑ {capacity}%";
+          interval = 5;
+          states = {
+            warning = 20;
+            critical = 10;
+          };
         };
 
         network = {
-          format-wifi = "󰖩 {signalStrength}%";
-          format-ethernet = "󰈀 ETH";
-          format-disconnected = "󰖪 NO NET";
+          format = "{icon}";
+          format-icons = [
+            "󰤯"
+            "󰤟"
+            "󰤢"
+            "󰤥"
+            "󰤨"
+          ];
+          format-wifi = "{icon}";
+          format-ethernet = "󰀂";
+          format-disconnected = "󰤮";
+          tooltip-format-wifi = "{essid} ({signalStrength}%)";
+          tooltip-format-ethernet = "Connected";
+          tooltip-format-disconnected = "Disconnected";
+          interval = 5;
           on-click = "kitty --class impala-float impala";
         };
 
         pulseaudio = {
-          format = "{icon} {volume}%";
-          format-muted = "󰖁 MUTE";
+          format = "{icon}";
+          format-muted = "";
           format-icons = {
+            headphone = "";
+            headset = "";
             default = [
-              "󰕿"
-              "󰖀"
-              "󰕾"
+              ""
+              ""
+              ""
             ];
           };
+          scroll-step = 5;
+          tooltip-format = "Playing at {volume}%";
           on-click = "pavucontrol";
+          on-click-right = "pamixer -t";
         };
 
         bluetooth = {
-          format = "󰂯 {status}";
-          format-disabled = "󰂲 OFF";
-          format-connected = "󰂱 {num_connections}";
+          format = "";
+          format-off = "󰂲";
+          format-disabled = "󰂲";
+          format-connected = "󰂱";
+          format-no-controller = "";
+          tooltip-format = "Devices connected: {num_connections}";
           on-click = "kitty --class bluetui-float bluetui";
         };
 
-        "custom/gh-pomodoro" = {
-          exec = "gh-pomodoro-status";
-          interval = 1;
+        "group/tray-expander" = {
+          orientation = "inherit";
+          drawer = {
+            transition-duration = 600;
+            children-class = "tray-group-item";
+          };
+          modules = [
+            "custom/expand-icon"
+            "tray"
+          ];
+        };
+
+        "custom/expand-icon" = {
+          format = "";
+          tooltip = false;
+        };
+
+        tray = {
+          icon-size = 13;
+          spacing = 12;
+        };
+
+        "custom/focus" = {
+          exec = "focus-mode status";
+          interval = 2;
           format = "{}";
-          on-click = "gh-pomodoro-stop";
+          tooltip-format = "Focus mode (Do Not Disturb) active";
+          on-click = "focus-mode toggle";
         };
       };
     };
     style = ''
       * {
-        font-family: "JetBrainsMono Nerd Font Propo", "DejaVu Sans", sans-serif;
-        font-size: 14px;
+        font-family: "JetBrainsMono Nerd Font", "JetBrainsMono Nerd Font Propo", "DejaVu Sans", sans-serif;
+        font-size: 16px;
         font-weight: 500;
         border: none;
         border-radius: 0;
+        min-height: 0;
       }
 
       window#waybar {
@@ -120,19 +211,32 @@ _:
         color: #ffffff;
       }
 
+      .modules-left {
+        margin-left: 8px;
+      }
+
+      .modules-right {
+        margin-right: 8px;
+      }
+
       #workspaces button {
-        padding: 0 8px;
-        background-color: transparent;
+        all: initial;
+        padding: 0 6px;
+        margin: 0 1.5px;
+        min-width: 9px;
         color: #888888;
       }
 
       #workspaces button.active {
-        color: #ffffff;
-        background-color: #383838;
+        color: #33ccff;
+      }
+
+      #workspaces button.empty {
+        opacity: 0.5;
       }
 
       #workspaces button:hover {
-        background-color: #2a2a2a;
+        color: #ffffff;
       }
 
       #window,
@@ -143,9 +247,32 @@ _:
       #network,
       #pulseaudio,
       #bluetooth,
-      #tray,
-      #custom-gh-pomodoro {
-        padding: 0 10px;
+      #custom-focus {
+        margin: 0 7.5px;
+      }
+
+      #custom-focus {
+        color: #f9e2af;
+      }
+
+      #tray {
+        margin-right: 16px;
+      }
+
+      #custom-expand-icon {
+        margin-right: 12px;
+      }
+
+      #battery.warning {
+        color: #f9e2af;
+      }
+
+      #battery.critical {
+        color: #f38ba8;
+      }
+
+      tooltip {
+        padding: 2px;
       }
     '';
   };
